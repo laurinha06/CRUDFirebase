e3ff0c3bf246852695a4cf252dacb5af8b2d1a3f
laurinha06 <132668199+laurinha06@users.noreply.github.com>
2024-03-28 13:08:31 -0300

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBg9TB7cGwDcsBp4qwZ1ffDKUcHM-jBKIQ",
+  authDomain: "marcela1.firebaseapp.com",
+  databaseURL: "https://ativmay-firebase-default-rtdb.firebaseio.com/",
+  projectId: "marcela1",
+  storageBucket: "marcela1.appspot.com",
+  messagingSenderId: "448339453256",
+  appId: "1:448339453256:web:eff0e490b3b0d92ce83f7d"
+};
+
+// Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
